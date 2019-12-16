0cb75634332693edd7031fb57fb4bf8fefc3fa5e
Hailey Gaylor <haileygaylor@gmail.com>
2019-12-15 16:45:54 -0800

    returning data on the river station number with href info through scrapper

--- backend/riverScrapper.js
+++ backend/riverScrapper.js
@@ -2,52 +2,53 @@
 
 // Is it possible to create a dictionary to assign the station numbers to rivers?
 
-const rp = require('request-promise');
-const url = 'https://waterdata.usgs.gov/wa/nwis/current/?type=flow';
+// const rp = require('request-promise');
+// const url = 'https://waterdata.usgs.gov/wa/nwis/current/?type=flow';
 
-rp(url)
-    .then(function (html) {
-        //success!
-        console.log($('tbody > a', html).length);
-        console.log($('tbody > a', html));
-    })
-    .catch(function (err) {
-        //handle error
-    });
-
-// const axios = require('axios');
-// const cheerio = require('cheerio');
-
-// const url = 'https://waterdata.usgs.gov/or/nwis/uv/?site_no=14142500&PARAmeter_cd=00065,00060';
-// //set up a dictionary for the river names and station number
-// // per the users choice, search the river name on the first url site then return that to the scrapper and replace .
-
-
-
-// axios(url)
-//     .then(response => {
-//         const html = response.data;
-//         const $ = cheerio.load(html)
-//         const riverContainerSearch = $('.statsTableContainer > td');
-//         const riverSearch = [];
-
-//         riverContainerSearch.each(function () {
-//             const riverName = $(this).find('body > h2').text();
-//             const dateTime = $(this).find('.stationContainer > text').text();
-//             const gageCfs = $(this).find('.highlight2').text();
-//             const gageFt = $(this).find('.stationContainer').text();
-
-//             riverSearch.push({
-//                 riverName: riverName,
-//                 dateTime: dateTime,
-//                 gageCfs: gageCfs,
-//                 gageFt: gageFt
-//             });
-//         });
-
-//         console.log(topPremierLeagueScorers);
+// rp(url)
+//     .then(function (html) {
+//         //success!
+//         console.log($('tr > a href', html).length);
+//         console.log($('tr > a href', html));
 //     })
-//     .catch(console.error);
+//     .catch(function (err) {
+//         //handle error
+//     });
+
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const url = 'https://waterdata.usgs.gov/wa/nwis/current/?type=flow';
+//set up a dictionary for the river names and station number
+// per the users choice, search the river name on the first url site then return that to the scrapper and replace .
+
+
+
+axios(url)
+    .then(response => {
+        const html = response.data;
+        const $ = cheerio.load(html)
+        const riverContainerSearch = $('td > a', html).length;
+        const riverSearch = [];
+
+        // riverContainerSearch.each(function () {
+        //     const riverName = $(this).find('body > h2').text();
+        //     const dateTime = $(this).find('.stationContainer > text').text();
+        //     const gageCfs = $(this).find('.highlight2').text();
+        //     const gageFt = $(this).find('.stationContainer').text();
+
+        //     riverSearch.push({
+        //         riverName: riverName,
+        //         dateTime: dateTime,
+        //         gageCfs: gageCfs,
+        //         gageFt: gageFt
+            // });
+            console.log(riverContainerSearch);
+            console.log($('td > a', html));
+        });
+
+    // })
+    // .catch(console.error);
 
 
 // TRYING TO SCRAP INFO ASSUMING DYNAMIC SITE -- was only able to return array. Not sure if I am targeting the right search querey.
